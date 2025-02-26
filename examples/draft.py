45c94ed4a4729ded613748ce32c719c9487e9141
Chuanrui Jiang <chuanrui@wustl.edu>
2025-02-25 23:42:28 -0600

    incorperated ball inclusion check (50/100) of the first condition check.

--- examples/draft.py
+++ examples/draft.py
@@ -1,10 +1,27 @@
 import numpy as np
 import pydrake.symbolic as sym
-import compatible_clf_union_cbf.union_cbf as mut
+import compatible_clf_union_cbf.ball_inclusion as ball_inclusion
 
 
 def main():
-    
+    x = sym.MakeVectorContinuousVariable(2, "x")
+    center = np.array([10, 0])
+    h = np.array([
+        sym.Polynomial(5 - (x - center).dot(x - center)),
+    ])
+    ball_inclustion_checking = ball_inclusion.BallInclusion(
+        r_start=10,
+        r_lower_bound=0.01,
+        polys=h,
+        x=x,
+    )
+    degrees = np.array([
+        [2, 2]
+    ])
+    checking_result = ball_inclustion_checking.ball_inclusion_verification(
+        degrees=degrees
+    )
+    assert checking_result
 
 
 if __name__ == "__main__":
